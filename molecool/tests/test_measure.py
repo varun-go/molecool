0eae94f79dafd2b33605417d299c207b1d8851e4
Varun Gopal <gopal@g.clemson.edu>
2020-05-27 16:50:33 -0400

    add fixtures to testing files

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -4,6 +4,7 @@ Unit and regression test for measure module.
 
 import molecool
 import numpy as np 
+import pytest
 
 def test_calculate_distance():
     """ Test to ensure that calculate_distance returns expected value."""
@@ -30,3 +31,14 @@ def test_calculate_angle():
     expected_angle = 90.0
 
     assert expected_angle == calculated_angle
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle", [
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45),
+    (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60  ),
+    (np.array([np.sqrt(3)/2, (1/2), 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 30),
+])
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+
+    assert expected_angle == pytest.approx(calculated_angle), F'{calculated_angle} {expected_angle}'
--- molecool/tests/test_molecule.py
+++ molecool/tests/test_molecule.py
@@ -5,16 +5,24 @@ import numpy as np
 import pytest
 import pdb
 
-def test_build_bond_list():
+# TEST FIXTURES
+@pytest.fixture()
+def methane_molecule():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+    coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+    return symbols, coordinates
+
+# TEST FUNCTIONS
+
+def test_move_methane(methane_molecule):
+    symbols, coordinates = methane_molecule
+
+    coordinates[0] += 5
+
+def test_build_bond_list(methane_molecule):
     ''' Tests the build_bond_list function. There are multiple sub-tests.'''
     
-    coordinates = np.array([
-        [1, 1, 1],
-        [2.4, 1, 1],
-        [-0.4, 1, 1],
-        [1, 1, 2.4],
-        [1, 1, -0.4],
-        ])
+    symbols, coordinates = methane_molecule
 
     bonds = molecool.build_bond_list(coordinates)
 
@@ -30,10 +38,10 @@ def test_build_bond_list():
     with pytest.raises(ValueError):
         bonds = molecool.build_bond_list(coordinates, min_bond=-1)
 
-def test_molecular_mass():
+def test_molecular_mass(methane_molecule):
     ''' Tests the molecular_mass function in molecule.py '''
 
-    symbols = ['C', 'H', 'H', 'H', 'H']
+    symbols, coordinates = methane_molecule
 
     calculated_mass = molecool.calculate_molecular_mass(symbols)
     
@@ -43,9 +51,11 @@ def test_molecular_mass():
     # actual_mass with absolute range on difference.
     assert calculated_mass == pytest.approx(actual_mass, abs=1e-2)
 
-def test_center_of_mass():
-    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
-    coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+def test_center_of_mass(methane_molecule):
+    # symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    # coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+
+    symbols, coordinates = methane_molecule
 
     center_of_mass = molecool.calculate_center_of_mass(symbols, coordinates)
 
