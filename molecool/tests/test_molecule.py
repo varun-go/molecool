78cfa66b1a4a0e2b860582d045a40b505de39912
Varun Gopal <gopal@g.clemson.edu>
2020-05-27 12:46:05 -0400

    HW3: add test for molecular mass function

--- molecool/tests/test_molecule.py
+++ molecool/tests/test_molecule.py
@@ -3,6 +3,7 @@
 import molecool
 import numpy as np
 import pytest
+import pdb
 
 def test_build_bond_list():
     ''' Tests the build_bond_list function. There are multiple sub-tests.'''
@@ -28,3 +29,16 @@ def test_build_bond_list():
     # Test 2 : Exception should be thrown in min bond length is < 0.
     with pytest.raises(ValueError):
         bonds = molecool.build_bond_list(coordinates, min_bond=-1)
+
+def test_molecular_mass():
+    ''' Tests the molecular_mass function in molecule.py '''
+
+    symbols = ['C', 'H', 'H', 'H', 'H']
+
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+    
+    actual_mass = 16.04
+
+    # assertion below checks if calculated_mass is approximately equal to 
+    # actual_mass with absolute range on difference.
+    assert calculated_mass == pytest.approx(actual_mass, abs=1e-2)
